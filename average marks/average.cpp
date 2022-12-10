68b060014fdf00472339ccdb36af87683f098422
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-12-10 19:51:26 +0800

    finished average

--- average marks/average.cpp
+++ average marks/average.cpp
@@ -4,29 +4,45 @@
 using namespace std;
 
 int main() {
-    int students, questions, mark;
+    int students, questions;
     int total = 0;
+    float average = 0;
+    int above80 = 0;
+    int totalMarks = 0;
 
     cout << "Enter the number of students: ";
     cin >> students;
 
+    string* names = new string[students];
+
     cout << "Enter the number of questions in the exam: ";
     cin >> questions;
 
-    for (int i = 0; i < students; i++) {
-        string name;
+    int* marks = new int[questions];
 
-        cout << "Enter the name of the student: ";
-        cin >> name;
+    for (int i = 0; i < students; i++) {
+        total = 0;
+
+        cout << "\nEnter the name of the student: ";
+        cin >> names[i];
 
         for (int j = 0; j < questions; j++) {
             cout << "Enter the marks for question no. " << j + 1 << ": ";
-            cin >> mark;
+            cin >> marks[j];
 
-            total += mark;
+            total += marks[j];
         }
-        cout << "Total marks for student named " << name << " is " << total << endl;
+        if (total >= 80) {
+            above80++;
+        }
+        cout << "Total marks for student named " << names[i] << " is " << total << endl;
+        totalMarks += total;
     }
+
+    float averageMarks = totalMarks / (float)students;
+
+    cout << "\nAverage marks for all students is " << averageMarks << endl;
+    cout << "No. of students with marks 80 and above is " << above80 << endl;
 
     return 0;
 }
